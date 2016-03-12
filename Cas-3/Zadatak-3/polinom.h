c9e6170ebf30bec77c490b13d71d88e64af7f92f
Luka <lukamilosevic11@gmail.com>
2016-03-12 22:39:16 +0100

    Create polinom.h

---
+++ Cas-3/Zadatak-3/polinom.h
@@ -0,0 +1,8 @@
+#define MAX_KOE 100
+
+void unos(int a[MAX_KOE], int* n); 
+void ispis_polinoma(int a[MAX_KOE], int n);
+int suma_polinoma(int a[MAX_KOE], int n, int b[MAX_KOE], int m, int c[MAX_KOE]);
+void mnoz_skalarom(int a[MAX_KOE], int n, int c);
+int vr_poly(int a[MAX_KOE], int n, int x);
+int mul_poly(int a[MAX_KOE], int n, int b[MAX_KOE], int m, int c[MAX_KOE]);
