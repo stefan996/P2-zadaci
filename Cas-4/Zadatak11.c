7f2c80e5283d8ba59a1851ce3e2d4f37865fe2ac
Luka <lukamilosevic11@gmail.com>
2016-03-21 01:22:01 +0100

    Create Zadatak11.c

---
+++ Cas-4/Zadatak11.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int parna_c(int n) {
+
+    int br = 0;
+
+    if (n < 10) {
+        if (n % 2 != 0) return n * 10;
+        else return n;
+    } else {
+        br = parna_c(n / 10);
+        if ((n % 10) % 2 != 0)
+            return (br * 10 + n % 10) * 10;
+        else
+            return br * 10 + n % 10;
+    }
+}
+
+int main() {
+
+    int n;
+
+    printf("Unesi broj:\n");
+    scanf("%d", &n);
+
+    printf("Novi broj je:\n");
+    printf("%d", parna_c(n));
+
+    printf("\n");
+
+    return 0;
+}
