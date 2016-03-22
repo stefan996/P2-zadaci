44962110d012b7788ffd09c2c8eab31617b50838
Luka <lukamilosevic11@gmail.com>
2016-03-23 00:14:39 +0100

    Update Rekurzija.c

--- Rekurzija.c
+++ Rekurzija.c
@@ -243,7 +243,54 @@ int main() {
 }*/
 
 //ZADATAK 8
+
 //ZADATAK 9
+/*
+#include<stdio.h>
+#define max 100
+
+int palindrom(int niz[max], int *k, int n);
+
+int main() {
+
+    int i, n, k, niz[max];
+
+    printf("Unesi n:\n");
+    scanf("%d", &n);
+
+    k = n - 1;
+
+    printf("Uneti niz:\n");
+    for (i = 0; i < n; i++)
+        scanf("%d", &niz[i]);
+
+    if (palindrom(niz, &k, n - 1))
+        printf("Jeste!");
+    else
+        printf("Nije!");
+    printf("\n");
+
+    return 0;
+
+}
+
+int palindrom(int niz[max], int *k, int n) {
+
+    if (n == 0) {
+        if (niz[*k] == niz[n]) {
+            (*k)--;
+            return 1;
+        } else
+            return 0;
+    } else {
+        palindrom(niz, k, n - 1);
+        if (niz[*k] == niz[n]) {
+            (*k)--;
+            return 1;
+        } else
+            return 0;
+    }
+}*/
 
 //ZADATAK 10
 /*
@@ -314,3 +361,84 @@ int main() {
 
     return 0;
 }*/
+
+//ZADATAK 12
+/*
+#include<stdio.h>
+#define MAX 100
+
+int max_br(int niz[MAX], int n);
+
+int main() {
+
+    int i, n, niz[MAX];
+
+    printf("Unesi n:\n");
+    scanf("%d", &n);
+
+    printf("Uneti niz:\n");
+    for (i = 0; i < n; i++)
+        scanf("%d", &niz[i]);
+
+    printf("Max je: %d", max_br(niz, n - 1));
+
+    printf("\n");
+
+    return 0;
+
+}
+
+int max_br(int niz[MAX], int n) {
+    int max;
+
+    if (n == 0)
+        max = niz[0];
+    else {
+        max = max_br(niz, n - 1);
+        if (niz[n] > max)max = niz[n];
+    }
+    return max;
+}*/
+
+//ZADATAK 13
+/*
+#include<stdio.h>
+#define MAX 100
+
+int skalar_proiz(int niz1[MAX], int niz2[MAX], int n);
+
+int main() {
+
+    int i, n, niz1[MAX], niz2[MAX];
+
+    printf("Unesi n:\n");
+    scanf("%d", &n);
+
+    printf("Uneti niz1:\n");
+    for (i = 0; i < n; i++)
+        scanf("%d", &niz1[i]);
+
+    printf("Uneti niz2:\n");
+    for (i = 0; i < n; i++)
+        scanf("%d", &niz2[i]);
+
+    printf("Skalarni proizvod je: %d", skalar_proiz(niz1, niz2, n - 1));
+
+    printf("\n");
+
+    return 0;
+
+}
+
+int skalar_proiz(int niz1[MAX], int niz2[MAX], int n) {
+
+    if (n == 0)
+        return niz1[n] * niz2[n];
+    else
+        return niz1[n] * niz2[n] + skalar_proiz(niz1, niz2, n - 1);
+}*/
+
+//ZADATAK 14
+//ZADATAK 15
+//ZADATAK 16
+//ZADATAK 17
