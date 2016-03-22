89ed04987e34e40030604a31d6a6836e6ed40bc5
Luka <lukamilosevic11@gmail.com>
2016-03-23 00:46:58 +0100

    Update Rekurzija.c

--- Rekurzija.c
+++ Rekurzija.c
@@ -440,5 +440,41 @@ int skalar_proiz(int niz1[MAX], int niz2[MAX], int n) {
 
 //ZADATAK 14
 //ZADATAK 15
+
 //ZADATAK 16
+/*
+#include<stdio.h>
+#define MAX 100
+
+int crtanje(int n, int raz);
+
+int main() {
+
+    int n;
+
+    printf("Unesi n:\n");
+    scanf("%d", &n);
+
+    crtanje(n, 0);
+
+    printf("\n");
+
+    return 0;
+
+}
+
+int crtanje(int n, int raz) {
+    int i;
+
+    if (n == 1) {
+        for (i = 0; i < raz; i++)printf(".");
+        printf("+");
+    } else {
+        crtanje(n - 1, raz + 1);
+        printf("\n");
+        for (i = 0; i < raz; i++)printf(".");
+        for (i = 0; i < n; i++)printf("+");
+    }
+}*/
+
 //ZADATAK 17
