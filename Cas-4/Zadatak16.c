8326b98b8723b5168062413db4df13fc554d25c4
Luka <lukamilosevic11@gmail.com>
2016-03-23 00:47:41 +0100

    Create Zadatak16.c

---
+++ Cas-4/Zadatak16.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#define MAX 100
+
+int crtanje(int n, int raz);
+
+int main() {
+
+    int n;
+
+    printf("Unesi n:\n");
+    scanf("%d", &n);
+
+    crtanje(n, 0);
+
+    printf("\n");
+
+    return 0;
+
+}
+
+int crtanje(int n, int raz) {
+    int i;
+
+    if (n == 1) {
+        for (i = 0; i < raz; i++)printf(".");
+        printf("+");
+    } else {
+        crtanje(n - 1, raz + 1);
+        printf("\n");
+        for (i = 0; i < raz; i++)printf(".");
+        for (i = 0; i < n; i++)printf("+");
+    }
+}
