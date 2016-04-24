7c416af92419ac6201fe57d843b739cbf7590eda
Luka <lukamilosevic11@gmail.com>
2016-04-25 00:26:56 +0200

    Create Zadatak5.c

---
+++ Cas-7/Zadatak5.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define MAX 100
+#define MAX_KAR 21
+
+int poredi(const void* a, const void* b);
+
+int main() {
+
+    int n, i, k;
+    char niz[MAX][MAX_KAR];
+
+    printf("Uneti n:\n");
+    scanf("%d", &n);
+
+    printf("Uneti niz:\n");
+    for (i = 0; i < n; i++)
+        scanf("%s", niz[i]);
+
+    qsort(niz, n, sizeof (char)*MAX_KAR, poredi);
+
+    for (i = 0; i < n; i++)
+        printf("%s ", niz[i]);
+
+    return 0;
+}
+
+int poredi(const void* a, const void* b) {
+    return strcmp((char*) a, (char*) b);
+}
