ec4b19f011794cb8963fe775fad48600087ea5b1
Luka <lukamilosevic11@gmail.com>
2016-04-26 19:51:45 +0200

    Update Sortiranje.c

--- Sortiranje.c
+++ Sortiranje.c
@@ -303,12 +303,350 @@ void umetni(STUDENT niz[MAX], int k) {
 }*/
 
 //ZADATAK 8
+/*
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define MAX 100
+#define MAX_KAR 21
+
+typedef struct {
+    float oc;
+    char ime[MAX_KAR], prez[MAX_KAR];
+} STUDENT;
+
+int poredi(const void* a, const void* b);
+
+int main(int argc, char** argv) {
+
+    int i = 0, j;
+    STUDENT niz[MAX];
+
+    if (argc != 2) {
+        printf("Greska!");
+        exit(0);
+    }
+
+    FILE *f = fopen(argv[1], "r");
+
+    if (f == NULL) {
+        printf("Greska!");
+        exit(0);
+    }
+
+    while (fscanf(f, "%s%s%f", niz[i].ime, niz[i].prez, &niz[i].oc) != EOF)
+        i++;
+
+
+    qsort(niz, i, sizeof (STUDENT), poredi);
+
+    for (j = 0; j < i; j++)
+        printf("%s %s %f\n", niz[j].ime, niz[j].prez, niz[j].oc);
+
+    return 0;
+}
+
+int poredi(const void* a, const void* b) {
+    STUDENT* n = (STUDENT*) a;
+    STUDENT* m = (STUDENT*) b;
+
+    return n->oc - m->oc;
+}*/
 
 //ZADATAK 9
+/*
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define MAX 100
+
+void insertion_sort(int niz[MAX], int n);
+void umetni(int niz[MAX], int k);
+int prebroj(int niz[MAX], int n);
+
+int main() {
+    int n, i, k, niz[MAX];
+
+    printf("Uneti n:\n");
+    scanf("%d", &n);
+
+    printf("Uneti niz:\n");
+    for (i = 0; i < n; i++)
+        scanf("%d", &niz[i]);
+
+    insertion_sort(niz, n);
+
+    printf("Sortiran niz:");
+    for (i = 0; i < n; i++)
+        printf("%d ", niz[i]);
+
+    printf("\n%d", prebroj(niz, n));
+
+    return 0;
+}
+
+void insertion_sort(int niz[MAX], int n) {
+    int i;
+    for (i = 1; i < n; i++)
+        umetni(niz, i);
+}
+
+void umetni(int niz[MAX], int k) {
+    int i, x, j;
+    x = niz[k];
+    for (i = 0; i < k; i++)
+        if (x < niz[i]) {
+            for (j = k; j > i; j--)
+                niz[j] = niz[j - 1];
+            niz[i] = x;
+            break;
+        }
+}
+
+int prebroj(int niz[MAX], int n) {
+    int i, br = 1, br_max = 1, max;
+    max = niz[0];
+    for (i = 1; i < n; i++) {
+        if (niz[i] != niz[i - 1]) {
+            if (br > br_max) {
+                br_max = br;
+                max = niz[i - 1];
+            }
+            br = 1;
+        } else
+            br++;
+    }
+
+    return max;
+}*/
 
 //ZADATAK 10
+/*
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define MAX 100
+
+void insertion_sort(int niz[MAX], int n);
+void umetni(int niz[MAX], int k);
+int rastojanje(int niz[MAX], int n, int *l, int *d);
+
+int main() {
+    int n, i, l, d, niz[MAX], k;
+
+    printf("Uneti n:\n");
+    scanf("%d", &n);
+
+    printf("Uneti niz:\n");
+    for (i = 0; i < n; i++)
+        scanf("%d", &niz[i]);
+
+    insertion_sort(niz, n);
+
+    printf("Sortiran niz:");
+    for (i = 0; i < n; i++)
+        printf("%d ", niz[i]);
+
+    k = rastojanje(niz, n, &l, &d);
+
+    printf("\nNa najmanjem rastojanju %d su brojevi %d i %d!", k, niz[l], niz[d]);
+
+    return 0;
+}
+
+void insertion_sort(int niz[MAX], int n) {
+    int i;
+    for (i = 1; i < n; i++)
+        umetni(niz, i);
+}
+
+void umetni(int niz[MAX], int k) {
+    int i, x, j;
+    x = niz[k];
+    for (i = 0; i < k; i++)
+        if (x < niz[i]) {
+            for (j = k; j > i; j--)
+                niz[j] = niz[j - 1];
+            niz[i] = x;
+            break;
+        }
+}
+
+int rastojanje(int niz[MAX], int n, int *l, int *d) {
+    int i, r;
+    r = niz[1] - niz[0];
+    *d = 1;
+    *l = 0;
+    for (i = 2; i < n; i++)
+        if ((niz[i] - niz[i - 1]) < r) {
+            r = niz[i] - niz[i - 1];
+            *l = i - 1;
+            *d = i;
+        }
+    return r;
+}*/
 
 //ZADATAK 11
+/*
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+#define MAX 100
+
+typedef struct {
+    int im, br;
+} RAZLOMAK;
+
+int poredi(const void* a, const void* b);
+
+int main(int argc, char** argv) {
+
+    int i = 0, j;
+    char c;
+    RAZLOMAK niz[MAX];
+
+    if (argc != 2) {
+        printf("Greska!");
+        exit(0);
+    }
+
+    FILE *f = fopen(argv[1], "r");
+
+    if (f == NULL) {
+        printf("Greska!");
+        exit(0);
+    }
+    
+    //zamisljeno je da se u datoteci unose razlomci u obliku 3/4...
+    while (fscanf(f, "%d%c%d", &niz[i].br, &c, &niz[i].im) != EOF)
+        i++;
+
+
+    qsort(niz, i, sizeof (RAZLOMAK), poredi);
+
+    for (j = 0; j < i; j++)
+        printf("%d/%d\n", niz[j].br, niz[j].im);
+
+    return 0;
+}
+
+int poredi(const void* a, const void* b) {
+
+    RAZLOMAK* n = (RAZLOMAK*) a;
+    RAZLOMAK* m = (RAZLOMAK*) b;
+
+    return (((n->br)*(m->im)-(m->br)*(n->im))*((n->im)*(m->im)));
+}*/
 
 //ZADATAK 12
+/*
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
+#define MAX 100
+#define MAX_KAR 21
+#define MAX_IDE 3
+
+typedef struct {
+    char ime[MAX_KAR], id_ime[MAX_IDE];
+    int id_br;
+} STUDENT;
+
+int poredi(const void* a, const void* b);
+int poredi1(const void* a, const void* b);
+int poredi2(const void* a, const void* b);
+int poredi3(const void* a, const void* b);
+
+int main(int argc, char** argv) {
+
+    int i = 0, j;
+    STUDENT niz[MAX], x;
+    STUDENT *k;
+
+    if (argc != 2) {
+        printf("Greska!");
+        exit(0);
+    }
+
+    FILE *f = fopen(argv[1], "r");
+
+    if (f == NULL) {
+        printf("Greska!");
+        exit(0);
+    }
+
+    while (fscanf(f, "%s %c%c%d", niz[i].ime, &niz[i].id_ime[0], &niz[i].id_ime[1], &niz[i].id_br) != EOF)
+        i++;
+
+    printf("Uneti studenta:\n");
+    scanf("%s %c%c%d", x.ime, &x.id_ime[0], &x.id_ime[1], &x.id_br);
+    printf("\n");
+
+    //sortira po broju
+    qsort(niz, i, sizeof (STUDENT), poredi);
+
+    //sortira po slovima
+    //qsort(niz, i, sizeof (STUDENT), poredi1);
+
+    //trazi studenta po broju u identifikaciji
+    k = (STUDENT*) bsearch(&x.id_br, niz, i, sizeof (STUDENT), poredi2);
+
+    //trazi studenta po slovima u identifikaciji
+    //k = (STUDENT*) bsearch(x.id_ime, niz, i, sizeof (STUDENT), poredi3);
+
+    for (j = 0; j < i - 1; j++)
+        printf("%s %s%d\n", niz[j].ime, niz[j].id_ime, niz[j].id_br);
+    printf("\n");
+
+    //ispisuje nadjenog studenta
+    if (k != NULL)
+        printf("%s %s%d\n", k->ime, k->id_ime, k->id_br);
+    else
+        printf("Nema takvog!\n");
+    return 0;
+}
+
+//sortira po brojevima
+
+int poredi(const void* a, const void* b) {
+
+    STUDENT* n = (STUDENT*) a;
+    STUDENT* m = (STUDENT*) b;
+
+    return n->id_br - m->id_br;
+}
+
+//sortira po slovima 
+
+int poredi1(const void* a, const void* b) {
+
+    STUDENT* n = (STUDENT*) a;
+    STUDENT* m = (STUDENT*) b;
+
+    return strcmp((n->id_ime), (m->id_ime));
+}
+
+//pretrazuje studenta po broju
+
+int poredi2(const void* a, const void* b) {
+
+    int* n = (int*) a;
+    STUDENT* m = (STUDENT*) b;
+
+    return *n - m->id_br;
+}
+
+//pretrazuje studenta po slovu
+
+int poredi3(const void* a, const void* b) {
+
+    char* n = (char*) a;
+    STUDENT* m = (STUDENT*) b;
+
+    return strcmp(n, m->id_ime);
+}*/
